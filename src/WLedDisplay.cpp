5d92bc65848c715e73fac5e0abe914265160a3cb
Klaus Ahrenberg <klaus.ahrenberg@web.de>
2022-07-06 07:27:39 +0200

    1.25

--- src/WLedDisplay.cpp
+++ src/WLedDisplay.cpp
@@ -1,27 +1,30 @@
-#include <Arduino.h>
-#include "WNetwork.h"
 #include "WLedDisplay.h"
+
+#include <Arduino.h>
+
+#include "WNetwork.h"
 
 #define APPLICATION "LedDisplay"
 #define VERSION "1.25"
 #define FLAG_SETTINGS 0x17
 #define DEBUG false
 
-#define PIN_STATUS_LED 13 //LED_BUILTIN //13
+#define PIN_STATUS_LED 13  // LED_BUILTIN //13
 
 WNetwork *network;
 WLedDisplay *display;
 
 void setup() {
   if (DEBUG) {
-		Serial.begin(9600);
-	}
-	network = new WNetwork(DEBUG, APPLICATION, VERSION, PIN_STATUS_LED, FLAG_SETTINGS);
-	display = new WLedDisplay(network);
-	network->addDevice(display);
+    Serial.begin(9600);
+  }
+  network =
+      new WNetwork(DEBUG, APPLICATION, VERSION, PIN_STATUS_LED, FLAG_SETTINGS);
+  display = new WLedDisplay(network);
+  network->addDevice(display);
 }
 
 void loop() {
   network->loop(millis());
-	delay(5);
+  delay(5);
 }
